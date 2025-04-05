5e743282d4afcf01daaa33cd63b5523728995b98
diazputri4 <diaz.putri.tif424@polban.ac.id>
2025-04-06 01:56:56 +0700

    Add files via upload

---
+++ data.c
@@ -0,0 +1,5 @@
+#include "kota.h"  
+#include "data.h"
+
+Kota daftarKota[MAX_KOTA];
+int jumlahKota = 0;
---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>     
+#include "menu.h"
+
+int main() {
+    menuUtama();
+    return 0;
+}
